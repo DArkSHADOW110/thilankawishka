9ebd6ac2418978aa8845bfdbdfa01c89290c220c
THILAN KAWISHKA <thilankhewawasam2@gmail.com>
2026-01-14 16:44:35 +0530

    Delete Scripts.js

--- Scripts.js
+++
@@ -1,4 +0,0 @@
-        const toggle = document.getElementById('darkmode-toggle');
-        toggle.addEventListener('change', () => {
-            document.body.classList.toggle('dark-mode');
-        });
